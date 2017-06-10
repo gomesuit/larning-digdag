20b0f9e83b323126c4d5f3d4d45715c6f6648242
gomesuit <gomesuit@gmail.com>
2017-06-11 02:38:48 +0900

    save

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 VAGRANTFILE_API_VERSION = '2'
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = 'bento/centos-7.2'
+  config.vm.box = 'bento/centos-7.3'
   config.ssh.forward_agent = true
   config.vm.synced_folder '.', '/home/vagrant/synced'
 
--- provision.sh
+++ provision.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -e
 
-sudo yum install -y git vim
+# sudo yum install -y git vim
 
-# anyenv
-git clone https://github.com/riywo/anyenv ~/.anyenv
-echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
+# # anyenv
+# git clone https://github.com/riywo/anyenv ~/.anyenv
+# echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
+# source ~/.bash_profile
 
-anyenv install ndenv
-source ~/.bash_profile
-ndenv install v7.10.0
+# # ndenv
+# anyenv install ndenv
+# source ~/.bash_profile
 
-#sudo yum install java-1.8.0-openjdk.x86_64
+# # node
+# ndenv install v7.10.0
+# ndenv global v7.10.0
+
+# java
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 
-git clone -b v0.9.12 https://github.com/treasure-data/digdag.git
+curl -o ~/bin/digdag --create-dirs -L "https://dl.digdag.io/digdag-0.9.12"
+chmod +x ~/bin/digdag
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
+
