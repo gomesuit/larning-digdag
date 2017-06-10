86d1bc04dc4b276ce5413cec4d5ec41e4ac7fe8d
gomesuit <gomesuit@gmail.com>
2017-06-11 02:10:11 +0900

    save

--- provision.sh
+++ provision.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+set -e
+
+sudo yum install -y git vim
 
 # anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
-exec $SHELL -l
+source ~/.bash_profile
 
+anyenv install ndenv
+source ~/.bash_profile
+ndenv install v7.10.0
 
+#sudo yum install java-1.8.0-openjdk.x86_64
+sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 
+git clone -b v0.9.12 https://github.com/treasure-data/digdag.git
