7d1bc36e43fc43aa312e3e15c32bb9efd17dbef1
gomesuit <gomesuit@gmail.com>
2017-06-18 04:06:21 +0900

    save

--- provision.sh
+++ provision.sh
@@ -54,3 +54,14 @@ cp /home/vagrant/synced/settings/digdag.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable digdag
 systemctl start digdag
+
+
+# install docker
+curl -fsSL https://get.docker.com/ | sh
+systemctl enable docker
+systemctl start docker
+usermod -a -G docker vagrant
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
